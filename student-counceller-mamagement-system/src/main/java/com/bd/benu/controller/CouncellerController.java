28abcc68bf2add1daa9b921e149a3e6cb955aaea
bdbenu <bdbenu18@gmail.com>
2024-08-16 05:35:18 +0530

    code Change

--- student-counceller-mamagement-system/src/main/java/com/bd/benu/controller/CouncellerController.java
+++ student-counceller-mamagement-system/src/main/java/com/bd/benu/controller/CouncellerController.java
@@ -1,4 +1,4 @@
-package com.bd.benu;
+package com.bd.benu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ student-counceller-mamagement-system/src/main/java/com/bd/benu/controller/CounselorRestController.java
@@ -0,0 +1,8 @@
+package com.bd.benu.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CounselorRestController {
+
+}
